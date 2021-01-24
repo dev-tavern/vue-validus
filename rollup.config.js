e6bc640ace7247d5abc4d9e5b84c7c7a1323acff
joshuagamble <jogamble@joshgamble.org>
2021-01-23 22:42:50 -0500

    re-add umd bundle build

--- rollup.config.js
+++ rollup.config.js
@@ -55,7 +55,9 @@ function createEntry(config) {
 
 // CommonJS (for Node)
 // ES module (for bundlers)
+// UMD (for browser)
 export default createEntries([
   { input: 'src/index.ts', file: pkg.main, format: 'cjs' },
-  { input: 'src/index.ts', file: pkg.module, format: 'es' }
+  { input: 'src/index.ts', file: pkg.module, format: 'es' },
+  { input: 'src/index.ts', file: pkg.browser, format: 'umd' }
 ])
